066e5c3d3e4324fd229986f5ffe76e8a43bcd829
s19787 <s19787@pjwstk.edu.pl>
2024-04-15 18:35:34 +0200

    Template

---
+++ WebApplication2/Controllers/AnimalController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.DTOs;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WebApplication2.Controllers;
+
+[ApiController]
+[Route("api/animals")]
+
+public class AnimalController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<AnimalDTO>> GetAnimals()
+    {
+       using SqlConnection sqlConnection = new SqlConnection("Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True");
+
+       using SqlCommand sqlCommand = new SqlCommand();
+
+        sqlCommand.Connection = sqlConnection;
+
+        sqlCommand.CommandText = "SELECT * from  Animal";
+        sqlConnection.Open();
+
+        SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+        List<AnimalDTO> animals = new List<AnimalDTO>();
+        
+        while (sqlDataReader.Read())
+        {
+            AnimalDTO animal = new AnimalDTO();
+            animal.Id = (int)sqlDataReader["id"];
+            animal.Name = (string)sqlDataReader["name"];
+            animal.Description = (string)sqlDataReader["description"];
+            animal.Area = (string)sqlDataReader["area"];
+            animal.Category = (string)sqlDataReader["category"];
+            
+            animals.Add(animal);
+        }
+        
+        return Ok();
+    }
+}
---
+++ WebApplication2/Repositories/IAnimalRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repositories;
+
+public interface IAnimalRepository
+{
+    IEnumerable<Animal> GetAnimals();
+    Animal GetAnimalById(int id);
+    public int CreateAnimal(Animal animal);
+    public int DeleteAnimal(int id);
+    public int UpdateAnimal(Animal animal);
+}
